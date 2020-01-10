cb28597e4b963e04eace4697cb6804a788c9491b
CfrancCyrille <francois.cyrille@gmail.com>
2020-01-10 08:43:38 +0100

    Re-Init TP

---
+++ src/main/java/enigma/Machine.java
@@ -0,0 +1,80 @@
+package enigma;
+
+public class Machine {
+
+	private Rotor leftRotor;
+	private Rotor middleRotor;
+	private Rotor rightRotor;
+	private Reflector reflector;
+
+	public void initRotors(Reflector reflector, Rotor left, Rotor middle, Rotor right) {
+		this.reflector = reflector;
+		leftRotor = left;
+		middleRotor = middle;
+		rightRotor = right;
+	}
+
+	public void setPositions(String setting) {
+		char[] charSettings = setting.toCharArray();
+		reflector.setPosition(Rotor.toIndex(charSettings[0]));
+		leftRotor.setPosition(Rotor.toIndex(charSettings[1]));
+		middleRotor.setPosition(Rotor.toIndex(charSettings[2]));
+		rightRotor.setPosition(Rotor.toIndex(charSettings[3]));
+	}
+	
+	public void configure(Reflector reflector, Rotor left, Rotor middle, Rotor right, String setting) {
+		this.initRotors(reflector, left, middle, right);
+		this.setPositions(setting);
+
+	}
+
+	public String convert(String msg) {
+		msg = msg.toUpperCase();
+		char[] msgChars = msg.toCharArray();
+		String result = "";
+		for (char c : msgChars) {
+			result += convertChar(c);
+		}
+		return result;
+	}
+
+	char convertChar(char c) {
+		advanceRotors();
+		int charIndex = Rotor.toIndex(c);
+		int output;
+		output = rightRotor.convertForward(charIndex);
+		output = middleRotor.convertForward(output);
+		output = leftRotor.convertForward(output);
+		output = reflector.convertForward(output);
+		output = leftRotor.convertBackward(output);
+		output = middleRotor.convertBackward(output);
+		output = rightRotor.convertBackward(output);
+		return Rotor.toLetter(output);
+
+	}
+
+	void advanceRotors() {
+		boolean advanceLeft = false;
+		boolean advanceMiddle = false;
+		boolean advanceRight = true;
+		if (leftRotor.atNotch()) {
+		}
+		if (middleRotor.atNotch()) {
+			advanceMiddle = true;
+			advanceLeft = true;
+		}
+		if (rightRotor.atNotch()) {
+			advanceMiddle = true;
+			advanceRight = true;
+		}
+		if (advanceLeft) {
+			leftRotor.advance();
+		}
+		if (advanceRight) {
+			rightRotor.advance();
+		}
+		if (advanceMiddle) {
+			middleRotor.advance();
+		}
+	}
+}
---
+++ src/test/java/enigma/MachineTest.java
@@ -0,0 +1,35 @@
+package enigma;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import app.Main;
+
+public class MachineTest {
+	
+	String config ="* B III IV I AXLE";
+	String msg1 = "FROM his shoulder Hiawatha";
+	String code1 = "HYIHBSBOEBTWJZSXMWQKYGWSFR";
+	String msg2 = "HYIHLBKOMLIUYDCMPPSFSZW";
+	String code2 = "FROMHISSHOULDERHIAWATHA";
+	
+	@Test
+	public void convert_Msg1_code1() {
+		Machine m = new Machine();
+		Main.configure(m, config);
+		String expected = code1;
+		String actual = m.convert(msg1);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void unconvert_Msg2_code2() {
+		Machine m = new Machine();
+		Main.configure(m, config);
+		String expected = code2;
+		String actual = m.convert(msg2);
+		assertEquals(expected, actual);
+	}
+
+}
